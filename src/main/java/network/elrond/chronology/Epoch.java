5b79596b8e4fcbfe59ca016ac141e60dc5848e17
Iulian Pascalau <iulian.pascalau@gmail.com>
2018-05-11 15:44:04 +0300

    Added comments on chronology package

--- src/main/java/network/elrond/chronology/Epoch.java
+++ src/main/java/network/elrond/chronology/Epoch.java
@@ -1,15 +1,28 @@
 package network.elrond.chronology;
 
-import network.elrond.consensus.Validator;
-
 import java.util.ArrayList;
 import java.util.List;
+import network.elrond.consensus.Validator;
+
+/**
+ * The Epoch class implements the epoch time frame in which Rounds are being created
+ *
+ * @author  Elrond Team - JLS
+ * @version 1.0
+ * @since   2018-05-11
+ */
 
 public class Epoch {
+    //waiting validators
     private List<Validator> listWaiting;
+    //eligible validators (ready to be elected)
     private List<Validator> listEligible;
+    //the list of Rounds
     private List<Round> listRounds;
 
+    /**
+     * Default constructor
+     */
     public Epoch()
     {
         listWaiting = new ArrayList<Validator>();
@@ -17,6 +30,10 @@ public class Epoch {
         listRounds = new ArrayList<Round>();
     }
 
+    /**
+     * Method used to create a new Round object. Note the double referencing
+     * @return the newly created Round
+     */
     public Round createRound(){
         Round r = new Round(this);
         listRounds.add(r);
@@ -24,6 +41,10 @@ public class Epoch {
         return(r);
     }
 
+    /**
+     * Gets the last Round from the list
+     * @return the last round as Round
+     */
     public Round getLastRound()
     {
         if (listRounds.size() == 0) {
@@ -33,6 +54,10 @@ public class Epoch {
         return (listRounds.get(listRounds.size() - 1));
     }
 
+    /**
+     * Gets the eligible validators list
+     * @return the eligible validators as List
+     */
     public List<Validator> getEligibleList()
     {
         return (listEligible);
--- src/main/java/network/elrond/chronology/Round.java
+++ src/main/java/network/elrond/chronology/Round.java
@@ -1,19 +1,30 @@
 package network.elrond.chronology;
 
-import network.elrond.consensus.Validator;
-import network.elrond.core.Util;
-
-
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
+import network.elrond.consensus.Validator;
+
+/**
+ * The Round class implements the time frame in which a new Block can be committed
+ *
+ * @author  Elrond Team - JLS
+ * @version 1.0
+ * @since   2018-05-11
+ */
 
 public class Round {
-
+    //te validators list used in block-consensus
     private List<Validator> listValidators;
+    //Epoch container
     private Epoch parentEpoch;
+    //Round ID (a.k.a height)
     private BigInteger roundHeight;
 
+    /**
+     * Explicit constructor
+     * @param parentEpoch as Epoch
+     */
     public Round(Epoch parentEpoch) {
         listValidators = new ArrayList<Validator>();
         this.parentEpoch = parentEpoch;
@@ -28,14 +39,26 @@ public class Round {
         }
     }
 
+    /**
+     * Gets the parent Epoch (container)
+     * @return the container as Epoch
+     */
     public Epoch getParentEpoch() {
         return (parentEpoch);
     }
 
+    /**
+     * Gets the list of validators used in consensus
+     * @return the list of validators as List
+     */
     public List<Validator> getListValidators() {
         return (listValidators);
     }
 
+    /**
+     * Gets the Round ID (height)
+     * @return the Round height as BigInteger
+     */
     public BigInteger roundHeight() {
         return (roundHeight);
     }
