782bda852d6c1e9042494d7bda39c13b7ef37ca1
Iulian Pascalau <iulian.pascalau@gmail.com>
2018-05-30 09:02:43 +0300

    EN-54 commit 6 - P2P test

---
+++ elrond-core/src/test/java/network/elrond/p2p/P2PTest.java
@@ -0,0 +1,107 @@
+package network.elrond.p2p;
+
+import junit.framework.TestCase;
+import network.elrond.Application;
+import network.elrond.application.AppContext;
+import network.elrond.application.AppState;
+import network.elrond.data.Transaction;
+import network.elrond.service.AppServiceProvider;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class P2PTest {
+
+    @Test
+    public void testOverride1() throws Exception {
+        AppContext context = new AppContext();
+        context.setMasterPeerIpAddress("127.0.0.1");
+        context.setMasterPeerPort(4000);
+        context.setPort(4001 /*+ new Random().nextInt(10000)*/);
+        context.setPeerId(0);
+
+
+        Application app = new Application(context);
+        app.start();
+
+        AppState state = app.getState();
+
+        P2PObjectService p2PObjectService = AppServiceProvider.getP2PObjectService();
+
+        p2PObjectService.putJSONencoded("string1", "AAAA", state.getConnection());
+
+        String strGet1 = p2PObjectService.get(state.getConnection(), "AAAA").toString();
+
+        TestCase.assertEquals("\"string1\"", strGet1);
+
+        p2PObjectService.putJSONencoded("string2", "AAAA", state.getConnection());
+
+        String strGet2 = p2PObjectService.get(state.getConnection(), "AAAA").toString();
+        TestCase.assertEquals("\"string2\"", strGet2);
+
+        @SuppressWarnings("resource")
+        Scanner input = new Scanner(System.in);
+        while (input.hasNext()) {
+            if (input.nextLine().equals("exit")) {
+                app.stop();
+            }
+        }
+    }
+
+    public void testOverrideWriter() throws Exception {
+        AppContext context = new AppContext();
+        context.setMasterPeerIpAddress("127.0.0.1");
+        context.setMasterPeerPort(4000);
+        context.setPort(4000);
+        context.setPeerId(1);
+        context.setStorageBasePath("producer");
+
+
+        Application app = new Application(context);
+        app.start();
+
+        AppState state = app.getState();
+
+        P2PObjectService p2PObjectService = AppServiceProvider.getP2PObjectService();
+
+        @SuppressWarnings("resource")
+        Scanner input = new Scanner(System.in);
+        while (state.isStillRunning()) {
+
+            p2PObjectService.putJSONencoded("string1", "AAAA", state.getConnection());
+
+            Thread.sleep(1000);
+
+            p2PObjectService.putJSONencoded("string2", "AAAA", state.getConnection());
+
+            Thread.sleep(1000);
+        }
+    }
+
+    public void testOverrideReader() throws Exception {
+        AppContext context = new AppContext();
+        context.setMasterPeerIpAddress("127.0.0.1");
+        context.setMasterPeerPort(4000);
+        context.setPort(4001 /*+ new Random().nextInt(10000)*/);
+        context.setPeerId(0);
+        context.setStorageBasePath("consumer");
+
+        Application app = new Application(context);
+        app.start();
+
+        AppState state = app.getState();
+
+        P2PObjectService p2PObjectService = AppServiceProvider.getP2PObjectService();
+
+        @SuppressWarnings("resource")
+        Scanner input = new Scanner(System.in);
+        while (state.isStillRunning()) {
+
+            String strGet = p2PObjectService.get(state.getConnection(), "AAAA").toString();
+            System.out.println(strGet);
+            Thread.sleep(100);
+        }
+    }
+}
