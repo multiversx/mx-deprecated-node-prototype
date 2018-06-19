4919dab308e58db9b1b8a18ea8ce800387eb7153
Andrei Tara <andrei@transylvanialabs.com>
2018-06-19 16:37:35 +0300

    EN-153 Transaction receipt

--- elrond-core/src/main/java/network/elrond/SeedNodeRunner.java
+++ elrond-core/src/main/java/network/elrond/SeedNodeRunner.java
@@ -3,7 +3,6 @@ package network.elrond;
 import network.elrond.account.AccountAddress;
 import network.elrond.application.AppContext;
 import network.elrond.core.ThreadUtil;
-import network.elrond.core.Util;
 import network.elrond.crypto.PrivateKey;
 import network.elrond.crypto.PublicKey;
 import network.elrond.data.BootstrapType;
