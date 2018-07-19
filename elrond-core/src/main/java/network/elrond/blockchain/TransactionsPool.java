22626dd1953a561e2a5654de213601fb49227226
AdoAdoAdo <anaridado@gmail.com>
2018-07-19 13:50:05 +0300

    cleanup some commented code

--- elrond-core/src/main/java/network/elrond/blockchain/TransactionsPool.java
+++ elrond-core/src/main/java/network/elrond/blockchain/TransactionsPool.java
@@ -14,8 +14,6 @@ import java.util.Map;
 
 public class TransactionsPool {
 
-    //public static final int WINDOW_SIZE = 5;
-
     private static final Logger logger = LogManager.getLogger(TransactionsPool.class);
 
     private Object locker = new Object();
@@ -27,27 +25,12 @@ public class TransactionsPool {
         List<String> newList = null;
 
         synchronized (locker){
-            //cleanTransactionList();
 
             newList = new ArrayList<>(transactions);
         }
 
-//        logger.debug("Returned the transactions: {}", newList);
-
         return (newList);
     }
-
-//    public void removeTransactions(List<String> hashes){
-//        Util.check(hashes != null, "hashes != null");
-//
-//        synchronized (locker){
-//            logger.debug("About to remove {} transactions from a total of {} transactions", hashes.size(), transactions.size());
-//
-//            transactions.removeAll(hashes);
-//
-//            logger.debug("Remained {} transactions", transactions.size());
-//        }
-//    }
 
     public boolean addTransaction(String transactionHash){
         Util.check(transactionHash != null, "transaction != null");
@@ -87,22 +70,6 @@ public class TransactionsPool {
         return(false);
     }
 
-//    private void cleanTransactionList() {
-//        logger.debug("About to clean transaction pool. There are {} transactions", transactions.size());
-//
-//        transactions.removeAll(lastTransactions.keySet());
-//
-//        logger.debug("Remained {} transactions", transactions.size());
-//    }
-
-    //public ArrayBlockingQueue<String> getTransactionPool() {
-    //    return (transactions);
-    //}
-
-    //public void add(String transaction) {
-    //    transactions.add(transaction);
-    //}
-
     public void addBlock(Block block) {
         Util.check(block != null, "block != null");
 
@@ -120,23 +87,6 @@ public class TransactionsPool {
             }
 
             transactions.removeAll(hashes);
-
-
-//            List<BigInteger> processedBlockNonces = map.keySet().stream().sorted().collect(Collectors.toList());
-//            BigInteger currentBlockNonce = block.getNonce();
-//            if (CollectionUtil.contains(processedBlockNonces, currentBlockNonce)) {
-//                return;
-//            }
-//
-//
-//            map.put(currentBlockNonce, hashes);
-//
-//            if (processedBlockNonces.size() + 1 > WINDOW_SIZE) {
-//                map.remove(processedBlockNonces.get(0));
-//                logger.debug("Removed from transaction pool block with nonce {}", processedBlockNonces.get(0));
-//            }
-
-            //cleanTransactionList();
         }
     }
 }
