c75c47c843c5b7a06ebbd707dbd568df81bf7abc
Iulian Pascalau <iulian.pascalau@gmail.com>
2018-07-23 11:03:51 +0300

    Fixing different round times 8, 12, 16, etc.

--- elrond-core/src/main/java/network/elrond/consensus/handlers/EndRoundHandler.java
+++ elrond-core/src/main/java/network/elrond/consensus/handlers/EndRoundHandler.java
@@ -1,6 +1,7 @@
 package network.elrond.consensus.handlers;
 
 import network.elrond.application.AppState;
+import network.elrond.benchmark.Statistic;
 import network.elrond.chronology.SubRound;
 import network.elrond.core.EventHandler;
 
@@ -8,5 +9,14 @@ public class EndRoundHandler implements EventHandler<SubRound> {
     @Override
     public void onEvent(AppState state, SubRound data) {
         state.getStatisticsManager().updateNetworkStats(state);
+
+        boolean isLastBlockFromCurentRound = (state.getBlockchain().getCurrentBlock() != null) &&
+                (state.getBlockchain().getCurrentBlock().getRoundIndex() == data.getRound().getIndex());
+
+        if (!isLastBlockFromCurentRound){
+            if (state.getStatisticsManager() != null) {
+                state.getStatisticsManager().addStatistic(new Statistic(0));
+            }
+        }
     }
 }
