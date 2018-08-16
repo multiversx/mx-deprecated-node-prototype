0a02a7cd87ad33013688b61443de9eff26941772
AdoAdoAdo <anaridado@gmail.com>
2018-08-16 17:19:54 +0300

    Fix LRU map using guava Cache

--- elrond-core/src/main/java/network/elrond/core/LRUMap.java
+++ elrond-core/src/main/java/network/elrond/core/LRUMap.java
@@ -1,25 +1,34 @@
 package network.elrond.core;
 
-import java.util.concurrent.ConcurrentHashMap;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 
 /**
  * Simple LRU map used for reusing lookup values.
  */
-public class LRUMap<K,V> extends ConcurrentHashMap<K,V> {
-
-    private static final long serialVersionUID = 1L;
-
+public class LRUMap<K, V> {
+    private Cache<K, V> cache;
     protected final int maxEntries;
 
     public LRUMap(int initialEntries, int maxEntries) {
-        super(initialEntries, 0.8f, 3);
         this.maxEntries = maxEntries;
+        cache = CacheBuilder.newBuilder().maximumSize(maxEntries).build();
     }
 
-/* todo: temporary removed during concurrent impl
-    @Override
-    protected boolean removeEldestEntry(Map.Entry<K,V> eldest) {
-        return size() > maxEntries;
+
+    public V get(K key) {
+        return cache.getIfPresent(key);
     }
-*/
+
+    public void put(K key, V value) {
+        cache.put(key, value);
+    }
+
+    public boolean contains(K key) {
+        return this.get(key) != null;
+    }
+
+    public void clear() {
+        cache.cleanUp();
+    }
 }
